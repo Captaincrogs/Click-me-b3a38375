190fcdd9b4bf1e6ffe2141ea9e106daa904ffb61
Captaincrogs <2003829@talnet.nl>
2020-02-03 11:31:28 +0100

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-//document.querySelector(".h1tag").innerHTML = "All the elements";{
-var matches = document.querySelectorAll("h1");{
-matches[0].style.color = "blue";
+
+
+  
+var matches = document.querySelector("h1");
+var button0;
+
+
+var matches2 = document.querySelector(".body");
+var button1;
+
+//return document.body.style.backgroundColor = kleur1;
+function myFunction(){
+
+    matches.style.backgroundColor= button0;
+    matches2.style.backgroundColor= button1;
+}
+function saveUserData(){
+    button0 = document.getElementById("kleur0").value;
+    button1 = document.getElementById("kleur1").value;
+
 }
 
-var matches = document.querySelectorAll(".h1tag");
-function myFunction(){    
-    var kleur = document.getElementById("kleur").value;
-return document.body.style.backgroundColor = kleur;
+console.log(calc(8,9));
+
+
+function calc(a,b)
+{
+    return a * b;
 }
-
-
-
-
